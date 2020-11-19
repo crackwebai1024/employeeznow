781a1aad53a657954fb18e1e102a8acd24aefad0
henry <wxiao1214@gmail.com>
2020-11-20 03:31:34 +0800

    resetpassword page responsive is solved

--- client/src/views/Auth/Password/ResetPasswordForm.js
+++ client/src/views/Auth/Password/ResetPasswordForm.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { router } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
+import { Grid, Box, Container, TextField, Avatar, Button } from '@material-ui/core';
+import MainButton from '@components/Element/Button/MainButton';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -29,7 +26,8 @@ const useStyles = makeStyles((theme) => ({
     cursor: 'pointer',
   },
   form: {
-    width: '30rem'
+    width: "100%",
+    padding: '0 2rem 0 2rem'
   },
   heading1: {
     ...theme.typography.h4,
@@ -37,12 +35,15 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     marginTop: 30,
+    width: '100%',
     marginBottom: 25,
   },
   container: {
     maxWidth: '600px',
     margin: 'auto',
-    background: 'white'
+    background: 'white',
+    padding: '2rem 0',
+    boxShadow: "0 0 4px 0 rgba(0,0,0,.08), 0 2px 4px 0 rgba(0,0,0,.12)",
   },
   linkContainer: {
     marginBottom: '8rem',
@@ -66,6 +67,7 @@ const Login = ({ actions, resetPassword, loading, slug }) => {
   const [error, setError] = useState('')
   const [success, setSuccess] = useState(false)
   const classes = useStyles();
+  const history = useHistory();
 
   const password = useRef({});
   password.current = watch('password', '');
@@ -74,8 +76,8 @@ const Login = ({ actions, resetPassword, loading, slug }) => {
     let token = window.location.pathname.split("/")[2];
     let data = {
       role: formData.role,
-      newPasswordConfirm : formData.password,
-      newPassword : formData.password,
+      newPasswordConfirm: formData.password,
+      newPassword: formData.password,
       token
     }
 
@@ -98,105 +100,123 @@ const Login = ({ actions, resetPassword, loading, slug }) => {
   return (
     <Container>
       <Grid container direction="column" width="sm" className={classes.container} alignItems="center">
-        <Grid item>
+
+        <Grid item xs={12}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
         </Grid>
-        <Grid item>
+
+        <Grid item xs={12}>
           <Typography variant="h1" className={classes.heading1}>
             Reset Password
           </Typography>
         </Grid>
 
-        <form onSubmit={(e) => e.preventDefault()} className={classes.form}>
-          <RadioGroup aria-label="role">
-            <Grid item container direction="row" justify="center">
-              <Grid item>
-                <FormControlLabel
-                  control={<Radio value="employer" />}
-                  label="EMPLOYER"
-                  name="role"
-                  id="employer"
-                  inputRef={register({ required: true })}
-                />
+        <Grid container item xs={12}>
+          <form onSubmit={(e) => e.preventDefault()} className={classes.form}>
+            <RadioGroup aria-label="role">
+              <Grid item container direction="row" justify="center" xs={12}>
+                <Grid item>
+                  <FormControlLabel
+                    control={<Radio value="employer" />}
+                    label="EMPLOYER"
+                    name="role"
+                    id="employer"
+                    inputRef={register({ required: true })}
+                  />
+                </Grid>
+                <Grid item>
+                  <FormControlLabel
+                    control={<Radio value="employee" />}
+                    label="EMPLOYEE"
+                    name="role"
+                    id="employee"
+                    inputRef={register({ required: true })}
+                  />
+                </Grid>
               </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={<Radio value="employee" />}
-                  label="EMPLOYEE"
-                  name="role"
-                  id="employee"
-                  inputRef={register({ required: true })}
-                />
-              </Grid>
-            </Grid>
-            <FormHelperText
-              error={errors.role ? true : false}
-              style={{ textAlign: 'center' }}
-            >
-              {errors.role ? 'Please select your role' : ''}
-            </FormHelperText>
-          </RadioGroup>
-
-          <Grid item xs={12}>
-            <PasswordInput
-              error={errors.password ? true : false}
-              name="password"
-              label="Password"
-              helperText={
-                errors.password ? 'Password must be munimum 8 characters' : ''
-              }
-              id="password"
-              autoComplete="password"
-              inputRef={register({
-                required: true,
-                minLength: 8,
-              })}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <PasswordInput
-              error={errors.passwordConfirm ? true : false}
-              label="Password Confirm"
-              helperText={
-                errors.passwordConfirm ? 'Passwords do not match' : ''
-              }
-              id="passwordConfirm"
-              name="passwordConfirm"
-              autoComplete="passwordConfirm"
-              inputRef={register({
-                validate: (value) =>
-                  value === password.current || 'The passwords do not match',
-              })}
-            />
-          </Grid>
-          <Grid item container>
-            <Grid xs={5}>
-            </Grid>
-            <Grid xs={2}></Grid>
-            <Grid xs={5}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                disabled={loading}
-                color="primary"
-                onClick={handleSubmit(onSubmit)}
-                className={classes.button}
+              <FormHelperText
+                error={errors.role ? true : false}
+                style={{ textAlign: 'center' }}
               >
-                Change Password
-              </Button>
+                {errors.role ? 'Please select your role' : ''}
+              </FormHelperText>
+            </RadioGroup>
+
+            <Grid item xs={12}>
+              <PasswordInput
+                error={errors.password ? true : false}
+                name="password"
+                fullWidth
+                size="small"
+                label="Password"
+                helperText={
+                  errors.password ? 'Password must be munimum 8 characters' : ''
+                }
+                id="password"
+                autoComplete="password"
+                inputRef={register({
+                  required: true,
+                  minLength: 8,
+                })}
+              />
             </Grid>
-          </Grid>
-          {/* errorMassge when authentication is failed */}
-          {error && (
-            <Grid item className={classes.invalidMessage}>
-              {error}
+
+            <Grid item xs={12}>
+              <PasswordInput
+                error={errors.passwordConfirm ? true : false}
+                label="Password Confirm"
+                helperText={
+                  errors.passwordConfirm ? 'Passwords do not match' : ''
+                }
+                id="passwordConfirm"
+                name="passwordConfirm"
+                autoComplete="passwordConfirm"
+                size="small"
+                inputRef={register({
+                  validate: (value) =>
+                    value === password.current || 'The passwords do not match',
+                })}
+              />
             </Grid>
-          )}
-        </form>
+
+            <Grid item container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Box className={classes.button}>
+                  <MainButton
+                    background="green"
+                    pd={20} fontSize={18}
+                    bd={3}
+                    label="Change Password"
+                    width="100%"
+                    hoverBack="#007000"
+                    onClick={handleSubmit(onSubmit)}
+                    border="green"
+                    color="white"
+                    hoverColor="white"
+                  >
+                  </MainButton>
+                </Box>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  color="primary"
+                  className={classes.button}
+                  onClick={e => history.push("/forgotPassword")}
+                  variant="outlined"
+                >Cancel</Button>
+              </Grid>
+            </Grid>
+            {/* errorMassge when authentication is failed */}
+            {error && (
+              <Grid item className={classes.invalidMessage}>
+                {error}
+              </Grid>
+            )}
+          </form>
+        </Grid>
+
       </Grid>
     </Container>
   );
