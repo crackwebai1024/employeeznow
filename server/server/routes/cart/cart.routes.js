822c8002234adb36b10dfcd98d4031217a76503c
crackwebai1024 <webtowardapex@outlook.com>
2020-12-04 08:14:39 +0800

    addtocart, delete from cart, get from cart, update search result

--- server/server/routes/cart/cart.routes.js
+++ server/server/routes/cart/cart.routes.js
@@ -4,7 +4,15 @@ import cartCtrl from "../../controllers/cart/cart";
 
 const router = express.Router();
 const { requireSignin, hasAuthorization } = authCtrl;
-const { addToCart } = cartCtrl;
+const { addToCart, readFromCart, deleteByID } = cartCtrl;
+
+// add one employee to the shopping cart
 router.route("/addtocart").post(requireSignin, hasAuthorization, addToCart);
+
+// get all employees in the shopping cart
+router.route("/read").get(requireSignin, hasAuthorization, readFromCart);
+
+// delete one employee in the shopping cart by id
+router.route("/deleteone").post(requireSignin, hasAuthorization, deleteByID);
 
 export default router;
